4e80e1020273feba9e5c4730bfbb1a7c393584c3
ItsSiapaAja <farandkenzie2000@gmail.com>
2022-08-30 19:43:07 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("IPS Club SMPN 1 Cibadak");
+
+let ips = document.getElementById("aips");
+let ssk = document.getElementById("assk");
+let header = 70;
+
+let buttonIPS = document.querySelector("div.jumbotron div.ips");
+let buttonSSK = document.querySelector("div.jumbotron div.ssk");
+
+buttonIPS.addEventListener("click", function (e) {
+    let ipsPos = ips.getBoundingClientRect().top;
+    let offsetPos = ipsPos + window.pageYOffset - header;
+    window.scrollTo({
+        top: offsetPos,
+        behavior: "smooth"
+    });
+});
+
+buttonSSK.addEventListener("click", function (e) {
+    let sskPos = ssk.getBoundingClientRect().top;
+    let offsetPos = sskPos + window.pageYOffset - header;
+    window.scrollTo({
+        top: offsetPos,
+        behavior: "smooth"
+    });
+});
